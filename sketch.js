122cdfc985a0dfbe0f24eca533c6580779683a63
Aryan-2o <63726905+Aryan-2o@users.noreply.github.com>
2020-08-12 16:01:49 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,77 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var box1, cube1;
+var backgroundImg,platform;
+var player, slingShot;
+
+function preload() {
+    backgroundImg = loadImage("sprites/bg.png");
+}
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+
+    ground = new Ground(600,height,1200,20);
+    platform = new Ground(150, 305, 300, 170);
+
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+    cube1 = new Cube(810, 350);
+    log1 = new Log(810,260,300, PI/2);
+
+    box3 = new Box(700,240,70,70);
+    box4 = new Box(920,240,70,70);
+    cube3 = new Cube(830, 350);
+
+    log3 =  new Log(810,180,300, PI/2);
+
+    box5 = new Box(810,160,70,70);
+    log4 = new Log(760,120,150, PI/7);
+    log5 = new Log(870,120,150, -PI/7);
+
+    player = new Player(200,50);
+
+    
+    slingshot = new SlingShot(player.body,{x:200, y:50});
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    strokeWeight(4);
+    box1.display();
+    box2.display();
+    ground.display();
+    cube1.display();
+    log1.display();
+
+    box3.display();
+    box4.display();
+    cube3.display();
+    log3.display();
+
+    box5.display();
+    log4.display();
+    log5.display();
+
+    player.display();
+    platform.display();
+
+    slingshot.display();    
+}
+
+function mouseDragged(){
+    Matter.Body.setPosition(player.body, {x: mouseX , y: mouseY});
+}
+
+
+function mouseReleased(){
+    slingshot.fly();
+}
